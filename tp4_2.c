1214f559f0d3c600856204dec2a11780353b8711
LucasAli18 <104177558+LucasAli18@users.noreply.github.com>
2023-05-02 15:18:34 -0300

    Punto 2

--- tp4_2.c
+++ tp4_2.c
@@ -10,11 +10,46 @@ struct
     int Duracion; // entre 10 – 100
 }typedef Tarea;
 
+void buscarTareaID(int id, int cantidad, Tarea **lista)
+{
+    int resultado =0;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (id == lista[i]->TareaID)
+        {
+            printf("\n La tarea obtenida es %s",lista[i]->Descripcion);
+            resultado = 1;  
+        }
+    }
+    if (resultado==0){
+        printf("\nNo se encontro tarea con ese id.");
+    }
+}
+
+void buscarTareaPalabra(char *palabra,int cantidad,Tarea **lista){
+   int resultado =0;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (strstr(lista[i]->Descripcion,palabra))
+        {
+            printf("\n La tarea obtenida es %s",lista[i]->Descripcion);
+            resultado = 1;  
+        }
+    }
+    if (resultado==0){
+        printf("\nNo se encontro tarea con esa palabra.");
+    }
+}
+
 int main()
 {
     srand(time(NULL));
 
         int canTareas, realizar,opcion=0;
+        int resultado,id;
+        char *palabraBuscada;
         printf("Indique la cantidad de tareas a cargar: ");
         scanf("%d",&canTareas);
         Tarea **ListaTareas;
@@ -36,7 +71,7 @@ int main()
             Tarea *TareaNueva;
             TareaNueva = (Tarea *) malloc(sizeof(Tarea)); 
             TareaNueva->TareaID=i+1;
-            TareaNueva->Descripcion=malloc(MAX*sizeof(char*)); 
+            TareaNueva->Descripcion=malloc(MAX*sizeof(char)); 
             printf("\n Descripcion de la tarea: ");
             fflush(stdin);
             gets(TareaNueva->Descripcion);
@@ -52,10 +87,11 @@ int main()
         printf("\n0.Salir");
         printf("\n1.Ver tareas pendientes");
         printf("\n2.Ver tareas realizadas");
+        printf("\n3.Buscar Tarea por ID");
+        printf("\n4.Buscar Tarea por palabra");
         printf("\n---------------------------------");
         printf("\n Elegir opcion: ");
         scanf("%d",&opcion);
-
 
         while (opcion!=0)
         {
@@ -88,7 +124,7 @@ int main()
                         }
                         
                     } 
-            }else if (opcion == 2)
+            }else if (opcion == 2){
                 for (int i = 0; i < canTareas; i++) 
                     {
                         if (TareasRealizadas[i]==NULL)
@@ -100,16 +136,48 @@ int main()
                             printf("\nLa duracion es %d",TareasRealizadas[i]->Duracion);
                         }
                     }
+            }else if (opcion==3)
+            {
+                printf("\n El ID buscado es: ");
+                scanf("%d",&id);
+                buscarTareaID(id,canTareas,ListaTareas);
+            }else if(opcion ==4)
+            {
+                palabraBuscada = (char *)malloc(MAX*sizeof(char));
+                printf("\n La palabra buscada es: ");
+                fflush(stdin);
+                gets(palabraBuscada);
+                buscarTareaPalabra(palabraBuscada,canTareas,ListaTareas);
+            }
+            
+                   
         printf("\n---------------------------------");
         printf("\n MENU");
         printf("\n0.Salir");
         printf("\n1.Ver tareas pendientes");
         printf("\n2.Ver tareas realizadas");
+        printf("\n3.Buscar Tarea por ID");
+        printf("\n4.Buscar Tarea por palabra");
         printf("\n---------------------------------");
         printf("\n Elegir opcion: ");
         scanf("%d",&opcion);
-        }
+
+}
 
 
     return 0;
 }
+
+/* 6. Cree un nuevo branch llamado busca-tarea e implemente una función de
+búsqueda de tarea por nro. de id de nombre BuscarTarea. La misma devuelve
+la tarea solicitada.
+7. Vuelva al branch main e implemente también una nueva versión de la función
+BuscarTarea en donde la misma sea por palabra clave en vez de por Id. (uno
+le manda una palabra y te tiene que devolver la primera tarea que contenga
+dicha palabra).
+Nota: Para realizar este punto, investigue el uso de la función strstr.
+8. Realizar el merge correspondiente y resuelva el conflicto producido.
+Finalmente modifique los nombres de las funciones a BuscaTareaPorId y otra
+BuscaTareaPorPalabra.
+9. Agregue una interfaz de usuario al programa principal que permita consultar
+tareas por id y palabra clave y mostrarlas por pantalla. */
